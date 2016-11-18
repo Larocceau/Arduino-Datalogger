997bead3c617549c06c5f1e6e84ce45ae921d302
Larocceau <joostkaptein@hotmail.nl>
2016-11-18 13:17:33 +0100

    Update ProjectDataLoggingV1.ino
    reformatted;
    made dots of points;

--- ProjectDataLoggingV1.ino
+++ ProjectDataLoggingV1.ino
@@ -10,6 +10,9 @@ int i = 0;
 int XscreenPoint[]= {18, 28, 38, 48, 58, 68, 78, 88, 98, 108, 118};
 int DataPoints[] = {0,0,0,30,40,50,60,23,64,12,34};
 int ScreenPoints[10];
+
+int ValMin = 0;
+int ValMax = 70;
 void setup() {
   // put your setup code here, to run once:
   TV.begin(PAL);
@@ -25,9 +28,13 @@ void loop() {
 void graphRefresh(){
   TV.clear_screen();
   graphSetup();
-  dataPointsToGraphPoints();
-  PlotScreenPoints();
-  ConnectTheDots();
+  drawGraph();
+}
+
+void drawGraph(){
+  dataPointsToGraphPoints(); 
+  connectTheDots();
+  plotScreenPoints();
 }
 
 void graphSetup(){
@@ -38,19 +45,19 @@ void graphSetup(){
 void dataPointsToGraphPoints(){
   i=0;
   for (i = 0; i < 11; i = i + 1){
-    int ScreenPoint = map(DataPoints[i], 0, 70, Ymax, Ymin);
+    int ScreenPoint = map(DataPoints[i], ValMin, ValMax, Ymax, Ymin);
     ScreenPoints[i]= ScreenPoint;
   }
 }
 
-void PlotScreenPoints(){
+void plotScreenPoints(){
   i = 0;
   for (i = 0 ; i < 10 ; i = i+1){
-    TV.set_pixel(XscreenPoint[i], ScreenPoints[i],1);
+    TV.draw_cirlce(XscreenPoint[i], ScreenPoints[i],2, 2,2);
   }
 }
 
-void ConnectTheDots(){
+void connectTheDots(){
   i =0;
   for (i =0; i<10;i=i+1){
     TV.draw_line(XscreenPoint[i], ScreenPoints[i], XscreenPoint[i+1], ScreenPoints[i+1],1);
